8e4060337579b91155beff415f472b1c85809445
Packet <aitorvadillo69@gmail.com>
2025-02-20 19:59:00 +0100

    Olvidona Scrapper done

--- Scrapper.py
+++ Scrapper.py
@@ -10,24 +10,25 @@ import time
 
 # basic setup
 options = webdriver.FirefoxOptions()
-# options.headless = True
+options.add_argument("--headless")
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
 driver.get("https://www.youtube.com/@TheWildProject")
 
-time.sleep(2)
+time.sleep(1.5)
 
 # accept cookies
 cookiesButton = driver.find_element(By.XPATH, '//*[contains(@aria-label, "Accept all")]')
 cookiesButton.click()
-time.sleep(2)
+time.sleep(1.5)
 
 # scroll into view to THE WILD PROJECT PODCAST
 podcastTitle = driver.find_element(By.XPATH, '//*[contains(@title, "THE WILD PROJECT PODCAST")]')
-print(podcastTitle.get_attribute("title"))
 driver.execute_script("arguments[0].scrollIntoView({behavior: 'auto', block: 'start'})", podcastTitle)
 
-# collect items from video container
-for it in range(1,6):
-    print(driver.find_element(By.XPATH,f'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[2]/div[3]/ytd-shelf-renderer/div[1]/div[2]/yt-horizontal-list-renderer/div[2]/div/div/ytd-grid-video-renderer[{it}]/div[1]/ytd-thumbnail/a').get_attribute("href"))
-
+# collect items from video container && write to file
+with open("WILD PROJECT PODCASTS.txt", "w") as file:
+    file.write("Latest WILD PROJECT PODCAST videos:\n")
+    for it in range(1,6):
+        link = driver.find_element(By.XPATH,f'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[2]/div[3]/ytd-shelf-renderer/div[1]/div[2]/yt-horizontal-list-renderer/div[2]/div/div/ytd-grid-video-renderer[{it}]/div[1]/ytd-thumbnail/a').get_attribute("href")
+        file.write(f"\t{it}. {link}\n")
    
